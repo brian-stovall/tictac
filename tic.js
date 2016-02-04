0ec83aae40ab1cd401cd1643e059374255b9f5fd
Brian Stovall <hoppenmaus@gmail.com>
2016-02-03 21:32:24 -0500

    basic ai code in place. needs bug-busting

--- tic.js
+++ tic.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
 	//make references to the DOM objects
 	var grab = document.getElementById.bind(document);
-	var board = grab('board-container');
-	var squares = board.getElementsByClassName('board-square');
+	var winBoard = grab('board-container');
+	var squares = winBoard.getElementsByClassName('board-square');
 
 	//images
 	var eksImage = 'assets/pusheen.png';
@@ -14,14 +14,15 @@ document.addEventListener('DOMContentLoaded', function() {
 	var empty = '_';
 
 	var boardState = initBoard();
-	var playerMarker = oh;
+	var playerMark = eks;
+	var aiMark = oh;
 
 	//center the board
-	board.style.top = (window.innerHeight - board.offsetHeight)/2 + 'px';
-	board.style.left = (window.innerWidth - board.offsetWidth)/2 + 'px';
+	winBoard.style.top = (window.innerHeight - winBoard.offsetHeight)/2 + 'px';
+	winBoard.style.left = (window.innerWidth - winBoard.offsetWidth)/2 + 'px';
 
 	//get an appropriate square size
-	var squareSize = board.offsetWidth/3;
+	var squareSize = winBoard.offsetWidth/3;
 
 	//set up the board squares
 	for (var i = 0; i < squares.length; i++) {
@@ -44,10 +45,22 @@ document.addEventListener('DOMContentLoaded', function() {
 		square.textContent = square.id;
 
 		square.onclick = function() {
-			if (placeMarker(playerMarker, boardState, this.id)) {
-				this.style['background-image'] = (playerMarker === eks) ?
+			if (placeMarker(playerMark, boardState, this.id)) {
+				this.style['background-image'] = (playerMark === eks) ?
 					'url(' + eksImage + ')' :
 					'url(' + ohImage + ')' ;
+				
+				if (checkWin(boardState, playerMark)) alert ('player wins');
+
+				//ai gets a move
+				var choice = aiChoice(boardState, aiMark, playerMark);
+				console.log('ai chooses: ' + choice);
+				placeMarker(aiMark, choice);
+				document.getElementById(choice).style['background-image'] = (aiMark === eks) ?
+					'url(' + eksImage + ')' :
+					'url(' + ohImage + ')' ;
+
+				if (checkWin(boardState, aiMark)) alert ('ai wins');
 			}
 		};
 
@@ -85,22 +98,72 @@ document.addEventListener('DOMContentLoaded', function() {
 
 		//now return all the corresponding values
 		for (i = 0; i < trips.length; i++)
-			trips[i] = trips[i].map( (val) => {return board.val;});
+			trips[i] = trips[i].map( (val) => {return [board.val, val];});
 
 		return trips;
 	}
 		
-	//check a single trip to see if it's one play away from being won
-	function checkTrip(trip, marker) {
-		var count = trip.map( (val) => {return (val === marker) ? 1 : 0;}).
-			reduce( (prev, curr) => {return (prev + curr)});
-		return (count === 2) ? true : false;
+	//check a single trip to see if it has a number of the marker
+	function countInTrip (trip, num, marker) {
+		var count = trip.map( (val) => {return (val[0] === marker) ? 1 : 0;})
+		if (count.length > 0) count = count.reduce( (prev, curr) => {return (prev + curr)});
+		return (count === num) ? true : false;
 	}
 
-	//returns a list of trips that are one move from being won
-	function checkTrips(board, marker) {
+	//returns a list of trips have num amount of a marker
+	function checkBoard(board, num, marker) {
 		var trips = getTrips(board);
-		return trips.map( (val) => {return checkTrip(val, marker);});
+		return trips.filter( (val) => {return countInTrip (val, num, marker);});
+	}
+
+	//composes the functions to look for a win for the marker
+	function checkWin (board, marker) {
+		return checkBoard(board, 3, marker).length > 0;
+	}
+
+	//looks for trips that have winning/losing moves
+	function getAtaris (board, marker) {
+		return checkBoard(board, 2, marker);
+	}
+
+	//returns an array of the possible moves from a trip
+	function getEmptyTrip(trip) {
+		return trip.filter( (val) => 
+			{return (val[0] === empty) ? true : false;}).
+			map( (entry) => {return entry[1]});
+	}
+
+	//returns the corners in a trip style
+	function getCorners(board) {
+		return [[board['00'], '00'], 
+						[board['20'], '20'],
+						[board['02'], '02'],
+						[board['22'], '22']];
+	}
+
+	//randomly choose an array element, or false for empty array
+	function randomArrayElem(arr) {
+		if (arr.length === 0) return false;
+		return arr[Math.floor(Math.random * (arr.length + 1))];
+	}
+
+  //ai method to make a choice when there are no
+	//wins or neccessary blocking moves
+	function aiDefaultChoice(board) {
+		//first, favor the middle if possible
+		if (board['11'] === empty) return '11';
+		else 
+			return (randomArrayElem(getEmptyTrip(getCorners(board))) || 
+				randomArrayElem(getEmptyTrip(getTrips(board))));
+	}
+
+	//ai function - first looks for a winning move, then a blocking one
+	//then chooses a default one
+	function aiChoice(board, aiMark, playerMark) {
+		return (
+			randomArrayElem(getAtaris(board, aiMark)) ||
+			randomArrayElem(getAtaris(board, playerMark)) ||
+			aiDefaultChoice(board));
 	}
 
 	//place a marker on the board
@@ -111,5 +174,6 @@ document.addEventListener('DOMContentLoaded', function() {
 			return true;
 		}
 	}
+
 //end game state code----------------------------------------------------------
 });
